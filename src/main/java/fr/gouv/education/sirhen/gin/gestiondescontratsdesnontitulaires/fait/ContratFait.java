11201d07ba79ea098f7c71ec2744e0cfe23e7b44
tromuald <tromuald@P12037.academie.in.ac-toulouse.fr>
2018-06-01 13:36:17 +0200

    ajout attributs des faits + TU

--- src/main/java/fr/gouv/education/sirhen/gin/gestiondescontratsdesnontitulaires/fait/ContratFait.java
+++ src/main/java/fr/gouv/education/sirhen/gin/gestiondescontratsdesnontitulaires/fait/ContratFait.java
@@ -7,6 +7,11 @@ import fr.gouv.education.sirhen.ct.moteurregles.commun.IFait;
 public class ContratFait implements IFait {
 
 	private Date dateDebutLienJuridique;
+	private Date dateFinPrevisionelleLienJuridique;
+	private Date dateFinReelLienJuridique;
+	private String typeContrat;
+	private String idContrat;
+	private String typeLienJuridique;
 
 	public Date getDateDebutLienJuridique() {
 		return dateDebutLienJuridique;
@@ -14,6 +19,58 @@ public class ContratFait implements IFait {
 
 	public void setDateDebutLienJuridique(final Date dateDebutLienJuridique) {
 		this.dateDebutLienJuridique = dateDebutLienJuridique;
+	}
+
+	public Date getDateFinPrevisionelleLienJuridique() {
+		return dateFinPrevisionelleLienJuridique;
+	}
+
+	public void setDateFinPrevisionelleLienJuridique(final Date dateFinPrevisionelleLienJuridique) {
+		this.dateFinPrevisionelleLienJuridique = dateFinPrevisionelleLienJuridique;
+	}
+
+	public Date getDateFinReelLienJuridique() {
+		return dateFinReelLienJuridique;
+	}
+
+	public void setDateFinReelLienJuridique(final Date dateFinReelLienJuridique) {
+		this.dateFinReelLienJuridique = dateFinReelLienJuridique;
+	}
+
+	public String getTypeContrat() {
+		return typeContrat;
+	}
+
+	public void setTypeContrat(final String typeContrat) {
+		this.typeContrat = typeContrat;
+	}
+
+	public String getIdContrat() {
+		return idContrat;
+	}
+
+	public void setIdContrat(final String idContrat) {
+		this.idContrat = idContrat;
+	}
+
+	/**
+	 * Accesseur en lecture de l'attribut <code>typeLienJuridique</code>.
+	 *
+	 * @return typeLienJuridique
+	 */
+	public String getTypeLienJuridique() {
+		return typeLienJuridique;
+	}
+
+	/**
+	 * Accesseur en écriture de l'attribut <code>typeLienJuridique</code>.
+	 *
+	 * @param typeLienJuridique
+	 *            String
+	 *
+	 */
+	public void setTypeLienJuridique(final String typeLienJuridique) {
+		this.typeLienJuridique = typeLienJuridique;
 	}
 
 }
