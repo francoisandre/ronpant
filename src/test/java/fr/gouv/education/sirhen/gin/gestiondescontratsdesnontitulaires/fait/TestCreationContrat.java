cda43ad9dfb8325179c73562883e202e8adbc5ad
fandre5 <fandre5@P12049.academie.in.ac-toulouse.fr>
2018-06-04 14:28:21 +0200

    Mise en place RAN_C_105

--- src/test/java/fr/gouv/education/sirhen/gin/gestiondescontratsdesnontitulaires/fait/TestCreationContrat.java
+++ src/test/java/fr/gouv/education/sirhen/gin/gestiondescontratsdesnontitulaires/fait/TestCreationContrat.java
@@ -160,7 +160,7 @@ public class TestCreationContrat extends AbstractTestRegles {
 		faits.add(autreContrat);
 
 		resultatExecution = moteurRegle.executerReglesGP(faits, Constantes.EVENEMENT_CREATION_CONTRAT);
-		Assert.assertFalse("La règle RAN_C_105 doit être non vérifiée", regleEstVerifiee("RAN_C_105", resultatExecution));
+		Assert.assertTrue("La règle RAN_C_105 doit être non vérifiée", regleEstNonVerifiee("RAN_C_105", resultatExecution));
 	}
 
 	/**
